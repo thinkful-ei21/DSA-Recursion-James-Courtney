b86a68a9391ac9c875da414c1ff9e6556dc53fd0
James Stuckey <jstuckey@rutgers.edu>
2018-07-23 17:51:20 -0400

    finished fibonacci

--- recursion.js
+++ recursion.js
@@ -133,4 +133,14 @@ const factorial = num => {
     return num * factorial(num - 1);
 };
 
-console.log(factorial(5));
+// console.log(factorial(5));
+
+const fibonacci = num => {
+    if (num === 1) return 1;
+
+    if (num === 0) return 0;
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
+};
+
+console.log(fibonacci(8));
